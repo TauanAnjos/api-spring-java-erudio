e24dc4948a40dc63e26b3d8611d415141c609134
TauanAnjos <tauandosanjosnascimento@gmail.com>
2024-04-22 10:36:52 -0300

    Tirando warnings

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -5,12 +5,13 @@ import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("persons")
+@RequestMapping("/persons")
 public class PersonController {
     @Autowired
     PersonService personService;
@@ -36,8 +37,9 @@ public class PersonController {
 
     }
     @DeleteMapping(value ="/{id}")
-    public void delete(@PathVariable(value = "id") Long id){
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
         personService.delete(id);
+        return ResponseEntity.noContent().build();
 
     }
 }
